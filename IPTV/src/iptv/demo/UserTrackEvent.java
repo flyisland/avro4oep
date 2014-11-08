a30081dcdbe260c2ef75a7bf7369f295a51c40d4
flyisland <flyisland@gmail.com>
2014-11-09 01:09:22 +0800

    generate aceesssors for UserTrackEvent

--- IPTV/src/iptv/demo/UserTrackEvent.java
+++ IPTV/src/iptv/demo/UserTrackEvent.java
@@ -12,4 +12,44 @@ public class UserTrackEvent {
     public UserTrackEvent() {
         super();
     }
+
+    public void setTrackTime(Date trackTime) {
+        this.trackTime = trackTime;
+    }
+
+    public Date getTrackTime() {
+        return trackTime;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setTargetURL(String targetURL) {
+        this.targetURL = targetURL;
+    }
+
+    public String getTargetURL() {
+        return targetURL;
+    }
+
+    public void setReferURL(String referURL) {
+        this.referURL = referURL;
+    }
+
+    public String getReferURL() {
+        return referURL;
+    }
+
+    public void setPageId(int pageId) {
+        this.pageId = pageId;
+    }
+
+    public int getPageId() {
+        return pageId;
+    }
 }
