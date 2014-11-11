9c67e2a9670d35c31d70f1ea5ad08a62ff622873
flyisland <flyisland@gmail.com>
2014-11-11 18:02:01 +0800

    add time stamp in the output message

--- IPTV/src/iptv/demo/AlertBean.java
+++ IPTV/src/iptv/demo/AlertBean.java
@@ -2,8 +2,10 @@ package iptv.demo;
 
 import com.bea.wlevs.ede.api.EventRejectedException;
 import com.bea.wlevs.ede.api.StreamSink;
+import java.util.Date; 
 
 public class AlertBean implements StreamSink {
+    java.text.DateFormat df = new java.text.SimpleDateFormat("hh:mm:ss"); 
     public AlertBean() {
         super();
     }
@@ -12,7 +14,8 @@ public class AlertBean implements StreamSink {
     public void onInsertEvent(Object event) throws EventRejectedException {
         if (event instanceof AlertEvent) {
             AlertEvent alert = (AlertEvent) event;
-            System.out.println(alert.toString());
+            
+            System.out.println(df.format(new Date())+"->"+alert.toString());
         }
     }
 }
