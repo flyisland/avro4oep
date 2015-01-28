a3c516797cfa8561b6783a06816037f17c008926
flyisland <flyisland@gmail.com>
2015-01-28 16:21:54 +0800

    add kafka test for 钟威

--- IPTV/src/iptv/demo/kafka/KafkaTest.java
+++ IPTV/src/iptv/demo/kafka/KafkaTest.java
@@ -1,5 +1,7 @@
 package iptv.demo.kafka;
 
+import java.io.IOException;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
@@ -14,7 +16,7 @@ public class KafkaTest {
         super();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         long events = 5;
         
         if (args.length == 1){
@@ -23,22 +25,16 @@ public class KafkaTest {
         Random rnd = new Random();
         
         Properties props = new Properties();
-        props.put("metadata.broker.list", "10.10.10.10:9092,10.10.10.10:9093");
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
-        props.put("partitioner.class", "iptv.demo.kafka.SimplePartitioner");
-        props.put("request.required.acks", "1");
-        
+        props.load(ClassLoader.getSystemResourceAsStream("kafka.properties"));  
         ProducerConfig config = new ProducerConfig(props);
         
         Producer<String, String> producer = new Producer<String, String>(config);
+        String json ="[{\"time\":\"20150127041043\", \"pageId\":1, \"area\":\"1\", \"pv\":2, \"uv\":1},\n" + 
+        "{ \"time\":\"20150127041043\", \"pageId\":2, \"area\":\"1\", \"pv\":1, \"uv\":1},\n" + 
+        "{ \"time\":\"20150127041043\", \"pageId\":4, \"area\":\"1\", \"pv\":2, \"uv\":1}]\n";        
         
-        for (long nEvents = 0; nEvents < events; nEvents++) { 
-               long runtime = new Date().getTime();  
-               String ip = "192.168.2." + rnd.nextInt(255); 
-               String msg = runtime + "www.example.com" + ip; 
-               KeyedMessage<String, String> data = new KeyedMessage<String, String>("page_visits", ip, msg);
-            producer.send(data);
-        }
+        KeyedMessage<String, String> data = new KeyedMessage<String, String>("browserStreamSec", json);
+        producer.send(data);
         producer.close();
     }
 }
