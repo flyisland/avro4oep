9780d7ad11addcf1c36211826e6b7017535dbc2d
flyisland <flyisland@gmail.com>
2015-01-28 16:23:06 +0800

    use kafka to ouput

--- IPTV/src/iptv/demo/BatchJSONBean.java
+++ IPTV/src/iptv/demo/BatchJSONBean.java
@@ -3,12 +3,35 @@ package iptv.demo;
 import com.bea.wlevs.ede.api.BatchStreamSink;
 import com.bea.wlevs.ede.api.EventRejectedException;
 
+import java.io.IOException;
+
 import java.util.Collection;
 import java.util.Date;
+
+import java.util.Properties;
+
+import kafka.javaapi.producer.Producer;
+
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
 
 public class BatchJSONBean implements BatchStreamSink {
     private java.text.DateFormat df = new java.text.SimpleDateFormat("yyyyMMddhhmmss"); 
     private String  etopic; // topic name of kafka
+    private Producer<String, String> producer = null;
+
+    public void init() {
+        if (producer == null){
+            Properties props = new Properties();
+            props.setProperty("metadata.broker.list", "itv-7:9092,itv-8:9092,itv-9:9092");
+            props.setProperty("producer.type", "async");
+            props.setProperty("compression.codec", "none");
+            props.setProperty("serializer.class", "kafka.serializer.StringEncoder");
+            props.setProperty("batch.num.messages", "100");
+            ProducerConfig config = new ProducerConfig(props);
+            producer = new Producer<String, String>(config);
+        }
+    }
 
     public void setEtopic(String etopic) {
         this.etopic = etopic;
@@ -36,7 +59,10 @@ public class BatchJSONBean implements BatchStreamSink {
             }
         }
         json.append("]");
-        System.out.println(json);
+        KeyedMessage<String, String> data = new KeyedMessage<String, String>(etopic, json.toString());
+        init();
+        producer.send(data);
+//        System.out.println(json);
     }
 
     @Override
@@ -46,13 +72,16 @@ public class BatchJSONBean implements BatchStreamSink {
             String eTime = df.format(new Date());
             PVUVEvent pvuv = (PVUVEvent) object;
             json.append(toJson(eTime, pvuv)).append("]");
-            System.out.println(json);
+            KeyedMessage<String, String> data = new KeyedMessage<String, String>(etopic, json.toString());
+            init();
+            producer.send(data);
+//          System.out.println(json);
         }
     }
     
     private StringBuilder toJson(String eTime, PVUVEvent pvuv) {
         StringBuilder  json = new StringBuilder("{");
-        json.append("\"type\":\"").append(etopic).append("\", \"time\":\"").append(eTime);
+        json.append("\"time\":\"").append(eTime);
         json.append("\", \"pageId\":").append(pvuv.getPageId()).append(", \"area\":\"").append(pvuv.getAreaId());
         json.append("\", \"pv\":").append(pvuv.getPv()).append(", \"uv\":").append(pvuv.getUv()).append("}");
         return json;
